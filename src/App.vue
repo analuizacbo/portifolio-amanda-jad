7ba6ce5284ddf59bb0ae9c0184505ee706401bfc
Ana <analu_cbo@hotmail.com>
2022-10-03 15:30:20 -0300

    Config project and import bootstrap ok

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Cabecario from './components/Cabecario.vue'
+
+
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <Cabecario />
   </header>
 
   <main>
-    <TheWelcome />
+    <router-view />
   </main>
 </template>
 
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  aroo
+</template>
