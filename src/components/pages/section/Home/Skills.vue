4fb1cdb3a76f35d5f3a6874ec2a9549b6927a452
Ana <analu_cbo@hotmail.com>
2022-10-08 05:39:41 -0300

    index compozation working

---
+++ src/components/pages/section/Home/Skills.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-center caixa-profissao">
+    <p class="txt-caveat-38">Eu sou UX Designer</p>
+    <p class="">Figma ° Adobe XD ° Photoshop ° Illustrator</p>
+    <p>Design Thinking, Design Strategy, Design Usability e Design System.</p>
+  </div>
+</template>
